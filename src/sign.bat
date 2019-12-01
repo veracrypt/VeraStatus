47f4d8cefe714b4ea6684bae86f8fc32c28b3d78
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2019-12-01 17:26:43 +0100

    Update sign.bat to use new IDRIX code signing certificates

--- src/sign.bat
+++ src/sign.bat
@@ -1,9 +1,9 @@
 PATH=%PATH%;%WSDK81%\bin\x86
 
 rem sign using SHA-1
-signtool sign /v /a /n "IDRIX" /i Thawte /ac Thawt_CodeSigning_CA.crt /fd sha1 /t http://timestamp.verisign.com/scripts/timestamp.dll Release\VeraStatus.exe x64\Release\VeraStatus.exe
+signtool sign /v /sha1 1FE67EF0455A9CC11433542FEC0A86DDD644B405 /ac DigiCert_High_Assurance_Code_Signing_CA.cer  /fd sha1 /t http://timestamp.verisign.com/scripts/timestamp.dll Release\VeraStatus.exe x64\Release\VeraStatus.exe
 
 rem sign using SHA-256
-signtool sign /v /a /n "IDRIX SARL" /i GlobalSign /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /as /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Release\VeraStatus.exe x64\Release\VeraStatus.exe
+signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /as /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Release\VeraStatus.exe x64\Release\VeraStatus.exe
 
 pause
